d822c03694e31bd47d95e7eca2112852b8434f2c
Andrew Cline <andrew.cline77@gmail.com>
2020-06-18 08:26:20 -0500

    refactor: Config::new implmentation to use iterator

--- src/lib.rs
+++ src/lib.rs
@@ -9,13 +9,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let filename = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Missing query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Missing filename"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
